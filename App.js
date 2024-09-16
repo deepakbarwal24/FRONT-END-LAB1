d7a3fe565e68745aa871a435c0db6b718f694641
deepakbarwal24 <deepakbarwal2497@gmail.com>
2024-09-15 23:35:57 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Greeter from './Greeter';
+
+function App() {
+  return(
+    <div>
+      <h1>Hello, React!</h1>
+      <Greeter />{/* use the greeter component*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ Greeter.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Greeter() {
+    return <h2>Hello, React!</h2>
+}
+export default Greeter;
